fb6107c6bd85ed3e8cf2afdbe7c87009e082c7ba
Gaurav Sutar <gauravstr05@gmail.com>
2023-01-24 14:05:12 +0530

    useRef explain #17

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import { createContext } from "react";
+// import { useEffect } from "react";
+import { useRef } from "react";
 import { useState } from "react";
 import "./App.css";
-import ComA from "./components/ComA";
+
 import Header from "./components/Header";
 
-const AppState = createContext();
-
 function App() {
-  const [data, setData] = useState("GauravStr26");
-  const [bio, setBio] = useState({
-    name: "Gaurav Sutar",
-    email: "gauravstr05@gmail.com",
-    age: 23,
-  });
+  const [input, setInput] = useState("");
+  // const counter = useRef(0);
+  let inputField = useRef("");
+
+  // useEffect(() => {
+  //   counter.current = counter.current + 1;
+  //   prevState.current = input;
+  // }, [input]);
+
+  const formHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const clickHandler = () => {
+    inputField.current.value = "Gaurav Sutar🥰";
+  };
+
   return (
     <div className="App">
-      <AppState.Provider value={{data, bio}}>
-        <Header />
-        <ComA />
-      </AppState.Provider>
+      <Header />
+      <input
+        ref={inputField}
+        className="input-box"
+        value={input}
+        onChange={formHandler}
+      />
+      {/* <h4>Application has been renderd {counter.current} times</h4> */}
+      {/* <h4>Your previous state was {prevState.current}</h4> */}
+      <button className="btn" onClick={clickHandler}>
+        Focus on input
+      </button>
     </div>
   );
 }
 
 export default App;
-export { AppState };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return <h1 className="header">Context API</h1>;
+  return <h1 className="header">Use Ref Hooks</h1>;
 };
 
 export default Header;
