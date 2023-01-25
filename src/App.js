2bf5ce49c4f90138523a6b3b523bc77058442a8b
Gaurav Sutar <gauravstr05@gmail.com>
2023-01-25 13:31:11 +0530

    React Loaders and toastify | React.js Full Course #22

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import { useCallback } from "react";
 import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Todos from "./components/Todos";
+import { ColorRing } from "react-loader-spinner";
+import { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [data, setData] = useState([]);
 
-  const increment = (c) => setCount((c) => c + 1);
-
-  const addTodo = useCallback(() => {
-    setTodos((t) => [...t, `New Todo ${t.length}`]);
-  }, [todos]);
+  useEffect(() => {
+    setLoader(true);
+    async function getData() {
+      const res = await fetch(
+        "https://hub.dummyapis.com/employee?noofRecords=150&idStarts=1001"
+      );
+      const finalRes = await res.json();
+      setLoader(false);
+      setData(finalRes);
+      toast.success("successfully fetched data");
+    }
+    getData();
+  }, []);
 
   return (
-    <div className="App">
-      <Header />
-      Count: {count}
-      <button className="btn" onClick={increment}>
-        +
-      </button>
-      <hr />
-      <Todos todos={todos} addTodo={addTodo} />
-    </div>
+    <>
+      <div className="App">
+        <Header />
+        <ToastContainer/>
+        {loader ? (
+          <div className="main">
+            <ColorRing color="#15202b" />
+          </div>
+        ) : (
+          data.map((element, index) => {
+            return <p key={index}>{element.firstName}</p>;
+          })
+        )}
+      </div>
+    </>
   );
 }
 
